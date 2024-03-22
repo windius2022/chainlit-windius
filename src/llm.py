9e165df9e65216ce7f2e5e60e626d50db59cf0a0
windius2022 <lloydst2020@gmail.com>
2024-03-22 16:11:52 -0700

    111

--- src/llm.py
+++ src/llm.py
@@ -1,16 +1,13 @@
 from openai import OpenAI
+from src.prompt import system_instruction
 
 client = OpenAI()
 
-response = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": "Who won the world series in 2020?"},
-    {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-    {"role": "user", "content": "Where was it played?"}
-  ]
-)
+messages = [
+    {"role": "system", "content": system_instruction}
+]
+
+
 def ask_order(messages, model="gpt-3.5-turbo",temperature=0):
     response = client.chat.completions.create(
         model= model,
