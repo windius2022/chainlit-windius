af1ed9f802c3eedc71f537367443847a2d1cde97
windius2022 <lloydst2020@gmail.com>
2024-03-22 15:47:09 -0700

    update  Changes to be committed: 	new file:   app.py 	new file:   requirements.txt 	new file:   setup.py 	new file:   src/__init__.py 	new file:   src/llm.py 	new file:   src/prompt.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'windius',
+    author_email= 'lloydst2020@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
